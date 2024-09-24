b0803487999bee639147736683532b3ecc6c67f9
Lucas Vasconcellos <lucasv.profissional@gmail.com>
2024-09-24 16:08:24 -0300

    Some function exercises
    All of the functions are in the bib.h library

--- Functions/APE_LE7.c
+++ Functions/APE_LE7.c
@@ -2,10 +2,14 @@
 #include <math.h>
 #include "bib.h"
 
-
+#define Pi 3.14
+#define QUAD(n) n*n
+#define Multiplo(a,b) (((a%b)==0) ? 1 : 0)
+#define Is_Vogal(letra) ((letra == 'A' || letra == 'a' || letra == 'E' || letra == 'e' || letra == 'I' || letra == 'i' || letra == 'O' || letra == 'o' || letra == 'U' || letra == 'u') ? 1 : 0)
+#define ABS(n) ((n>=0) ? n : (-1*(n)))
 
 int main(){
-    /*
+
     printf("\nExercicio 1\n");
     printf("\nPrograma do sinal\n");
 
@@ -66,14 +70,18 @@ int main(){
 
 
     printf("\nExercicio 6\n");
-    printf("\nPorcentagem de acrescimo\n");
-    
-    int produtoi, produtof;
+    printf("\nPrograma da media\n");
 
-    printf("Digite o valor inicial e final do produto separados por espaco: ");
-    scanf("%d %d", &produtoi, &produtof);
+    float n1, n2, n3;
+    char ope;
 
-    printf("A porcentagem de acrescimo do valor %d pra %d eh de %.1f%%", produtoi, produtof, acrescimo(produtoi, produtof));
+    printf("Digite tres notas separadas por espaco: ");
+    scanf("%f %f %f", &n1, &n2, &n3);
+
+    printf("Digite P para media ponderada A para aritmetica: ");
+    scanf(" %c", &ope);
+
+    printf("\nMedia %c das notas eh: %.2f\n\n", ope, media(ope, n1, n2, n3));
 
 
 
@@ -90,15 +98,123 @@ int main(){
     retangulo(altura, largura);
 
     printf("\n");
-    */
+    
+    
+
 
     printf("\nExercicio 8\n");
     printf("\nPrograma Verificador de cpf\n");
 
-    int cpf, verificador;
+    long long int cpfi;
 
-    printf("Digite o cpf e o digito verificador separados por espaco: ");
-    scanf("%d %d", &cpf, &verificador);
+    printf("Digite o cpf: ");
+    scanf("%lld", &cpfi);
+
+
+    digitos(cpfi);
+    
+
+
+    printf("\nExercicio 9\n");
+    printf("\nPrograma do Histograma\n");
+
+    int dom, seg, ter, qua, qui, sex, sab;
+
+    printf("Digite os graus Celsius de domingo: ");
+    scanf("%d", &dom);
+
+    printf("Digite os graus Celsius de segunda: ");
+    scanf("%d", &seg);
+
+    printf("Digite os graus Celsius de terca: ");
+    scanf("%d", &ter);
+
+    printf("Digite os graus Celsius de quarta: ");
+    scanf("%d", &qua);
+
+    printf("Digite os graus Celsius de quinta: ");
+    scanf("%d", &qui);
+
+    printf("Digite os graus Celsius de sexta: ");
+    scanf("%d", &sex);
+
+    printf("Digite os graus Celsius de sabado: ");
+    scanf("%d", &sab);
+
+
+    histograma(dom, seg, ter, qua, qui, sex, sab);
+    
+
+
+    printf("\nExercicio 10\n");
+    printf("\nArea do Circulo\n");
+
+    int raio;
+
+    printf("Digite o raio: ");
+    scanf("%d", &raio);
+
+    printf("A area de um circulo de raio %d eh: %.2fcm2", raio, area(raio));
+   
+    
+
+    printf("\nExercicio 11\n");
+    printf("\nQuadrado de um Numero\n");
+
+    int n;
+
+    printf("Digite um numero: ");
+    scanf("%d", &n);
+
+    printf("\nQuadrado de %d eh: %d\n\n", n, QUAD(n));
+    
+
+
+    printf("\nExercicio 12\n");
+    printf("\nPrograma do Ano Bissexto\n");
+
+    int ano;
+
+    printf("Digite um ano: ");
+    scanf("%d", &ano);
+
+    printf("\n%d eh bissexto?\nResposta: %d\n\n", ano, bissexto(ano));
+    
+
+
+    printf("\nExercicio 13\n");
+    printf("\nPrograma do Multiplo\n");
+
+    int a, b;
+
+    printf("Digite dois numeros separados por espaco: ");
+    scanf("%d %d", &a, &b);
+
+    printf("\n%d eh multiplo de %d bissexto?\nResposta: %d\n\n", a, b, Multiplo(a,b));
+    
+
+
+    printf("\nExercicio 14\n");
+    printf("\nPrograma da Vogal\n");
+
+    char letra;
+
+    printf("Digite uma letra: ");
+    scanf("%c", &letra);
+
+    printf("\n%c eh vogal?\nResposta: %d\n\n", letra, Is_Vogal(letra));
+    
+
+
+    printf("\nExercicio 15\n");
+    printf("\nPrograma do Absoluto\n");
+
+    int n;
+
+    printf("Digite um numero: ");
+    scanf("%d", &n);
+
+    printf("\nAbsoluto de %d eh: %d\n\n", n, ABS(n));
 
 
     return 0;
--- Functions/bib.h
+++ Functions/bib.h
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
+#define Pi 3.14
+
 int positivo(numero){
     if(numero >= 0){
         return 1;
@@ -54,3 +56,122 @@ void retangulo(altura, largura){
     }
 }
 
+void digitos(long long int cpfi){
+    long long int divisor, cpff, cpf;
+    int verificador, somatorio = 0;
+
+    cpf = cpfi;
+    cpff = cpfi;
+    for(int i = 9; i >= 0; i--){
+        divisor = (int)pow(10, i);
+        
+        somatorio = somatorio + (cpff/divisor);
+        cpff = cpff%divisor;
+        }
+
+    somatorio = (somatorio/10) * (somatorio%10);
+    verificador = ((somatorio%10)-(somatorio/10));
+
+    somatorio = 0;
+    cpff = (cpfi*10)+verificador;
+
+    for(int i = 10; i >= 0; i--){
+        divisor = (long long int)pow(10, i);
+        
+        somatorio = somatorio + (cpff/divisor);
+        cpff = cpff%divisor;
+    }
+
+    somatorio = (somatorio/10) * (somatorio%10);
+    verificador = (verificador*10) + ((somatorio%10)-(somatorio/10));
+    printf("O Cpf eh: %lld-%d", cpf, verificador);
+
+}
+
+void histograma(dom, seg, ter, qua, qui, sex, sab){
+    printf("\nHistograma:\n");
+
+    printf("D: ");
+    for(int i = 0; i < dom; i++){
+        printf("*");
+    }
+
+    printf("\n");
+    printf("S: ");
+    for(int i = 0; i < seg; i++){
+        printf("*");
+    }
+    printf("\n");
+
+    printf("T: ");
+    for(int i = 0; i < ter; i++){
+        printf("*");
+    }
+    printf("\n");
+
+    printf("Q: ");
+    for(int i = 0; i < qua; i++){
+        printf("*");
+    }
+    printf("\n");
+
+    printf("Q: ");
+    for(int i = 0; i < qui; i++){
+        printf("*");
+    }
+    printf("\n");
+
+    printf("S: ");
+    for(int i = 0; i < sex; i++){
+        printf("*");
+    }
+    printf("\n");
+
+    printf("S: ");
+    for(int i = 0; i < sab; i++){
+        printf("*");
+    }
+    printf("\n\n");
+}
+
+float area(raio){
+    return Pi * (raio*raio);
+}
+
+int bissexto(ano){
+    if((ano%4)==0){
+        if(ano%100){
+            if(ano%400){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+        else{
+            return 1;
+        }
+    }
+    else{
+        return 0;
+    }
+}
+
+float media(char ope, float n1, float n2, float n3){
+    float peso1 = 5, peso2 = 3, peso3 = 2;
+    switch (ope){
+    case 'A':            peso1 = 1;
+        peso2 = 1;
+        peso3 = 1;
+        break;
+    case 'P':
+        break;
+    default:
+        printf("Operacao invalida, digite de novo: ");
+        scanf("%c", &ope);
+        media(ope, n1, n2, n3);
+        break;
+    }
+
+    return ((n1*peso1) + (n2*peso2) + (n3*peso3))/(peso1+peso2+peso3);
+}
